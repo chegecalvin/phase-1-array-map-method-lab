1217fd2352e8e6b96151bd0478a9432e9ae34178
Calvin Chege <calvinchege1@gmail.com>
2024-04-09 18:29:56 +0300

    complete lab

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((sentence) => {
+    const words = sentence.split(" ");
+    const capitalizedWords = words.map((word) => word.charAt(0).toUpperCase() + word.slice(1));
+    const newSentence = capitalizedWords.join(" ");
+    return newSentence;
+  });
+};
